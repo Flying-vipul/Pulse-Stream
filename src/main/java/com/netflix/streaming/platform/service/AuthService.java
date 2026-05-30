b2dbf74b81c089be24af43d1e4a05e89d44b07c6
Vipul Choudhary <choudharyvipul722@gmail.com>
2026-05-30 22:08:20 +0530

    implemented the three layer arti(menu)

---
+++ src/main/java/com/netflix/streaming/platform/service/AuthService.java
@@ -0,0 +1,17 @@
+package com.netflix.streaming.platform.service;
+
+import com.netflix.streaming.platform.security.request.LoginRequest;
+import com.netflix.streaming.platform.security.request.SignupRequest;
+import com.netflix.streaming.platform.security.response.MessageResponse;
+import com.netflix.streaming.platform.security.response.UserInfoResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.multipart.MultipartFile;
+
+public interface AuthService {
+    UserInfoResponse authenticateUser(LoginRequest loginRequest);
+    MessageResponse registerUser(SignupRequest signupRequest);
+    boolean verifyOtp(String email, String otp);
+    String generateAndSetOtp(String email);
+    UserInfoResponse getUserDetails(Authentication authentication);
+    UserInfoResponse uploadAvatar(MultipartFile image, Authentication authentication);
+}
