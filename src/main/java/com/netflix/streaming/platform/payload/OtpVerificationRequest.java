bc86423014d14818dd1b752c7b1b48b4da2f6e5f
Vipul Choudhary <choudharyvipul722@gmail.com>
2026-06-01 23:19:39 +0530

    otp verification model

---
+++ src/main/java/com/netflix/streaming/platform/payload/OtpVerificationRequest.java
@@ -0,0 +1,14 @@
+package com.netflix.streaming.platform.payload;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class OtpVerificationRequest {
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    private String otp;
+}
